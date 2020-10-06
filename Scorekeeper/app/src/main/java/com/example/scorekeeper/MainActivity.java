df64e644d54b0829624746782af208331f86ec54
RashedRifat <59259308+RashedRifat@users.noreply.github.com>
2020-10-06 00:30:11 -0400

    Finished ScoreKeeperApp
    My first foray into extracting style resources for the entire app!

--- Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.scorekeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private int mScore2 = 0;
     private TextView mTextView1;
     private TextView mTextView2;
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView1 = (TextView) findViewById(R.id.score_1);
         mTextView2 = (TextView) findViewById(R.id.score_2);
+
+        // If the device has rotated, get the appropriate values
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            // Set the scores
+            mTextView1.setText(String.valueOf(mScore1));
+            mTextView2.setText(String.valueOf(mScore2));
+        }
     }
 
     public void decreaseScore(View view) {
@@ -44,6 +60,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // Check if the correct item was clicked
+        if(item.getItemId()==R.id.night_mode) {
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            recreate();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        }
+        else {
+            menu.findItem(R.id.night_mode).setTitle((R.string.night_mode));
+        }
+        return true;
+    }
+
     public void increaseScore(View view) {
         int viewID = view.getId();
 
@@ -56,5 +101,13 @@ public class MainActivity extends AppCompatActivity {
                 mScore2++;
                 mTextView2.setText(String.valueOf(mScore2));
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the Scores.
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
     }
 }
